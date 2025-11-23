981c70f1aabd014bab0845d953683b607092e189
Jonas <jonaseuler98@gmail.com>
2025-11-23 14:25:23 -0300

    setup app

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# from app.config import settings
+from api.controllers import generator_controller, reviewer_controller
+import json
+
+
+app = Flask(__name__)
+app.register_blueprint(generator_controller, url_prefix="/llm-generator")
+app.register_blueprint(reviewer_controller, url_prefix="/llm-reviewer")
+
+@app.route("/")
+def home():
+    return "App is running."
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
