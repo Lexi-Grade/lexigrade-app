05b5018e98abb6a451077f3a576a2efae65ae4bd
Jonas <jonaseuler98@gmail.com>
2025-12-02 11:46:25 -0300

    update settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -6,5 +6,7 @@ class Settings:
     def __init__(self):
         dotenv.load_dotenv()
         self.models_service_url = os.getenv('MODELS_SERVICE_URL')
-
+        self.hf_repo_spanish_url = os.getenv('HF_REPO_SPANISH_URL')
+        self.hf_repo_english_url = os.getenv('HF_REPO_ENGLISH_URL')
+        self.datasets_base_path = os.getenv('DATASETS_BASE_PATH')
 settings = Settings()
